c67fc5951d25bb17aeb1c7a68d067565999c9b00
Abdul <abdulmusty03@outlook.com>
2025-07-05 05:55:23 +0100

    chore: add select blacklist button

--- app/components/tables/data-table/index.tsx
+++ app/components/tables/data-table/index.tsx
@@ -1,7 +1,9 @@
 import { type Table as TanstackTable, flexRender } from "@tanstack/react-table";
+import { Trash2 } from "lucide-react";
 import type * as React from "react";
 
 import { DataTablePagination } from "~/components/tables/data-table/data-table-pagination";
+import { Button } from "~/components/ui/button";
 import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area";
 import {
     Table,
@@ -29,12 +31,13 @@ export function DataTable<TData>({
     return (
         <div
             className={cn(
-                "flex w-full flex-col gap-2.5 overflow-auto",
+                "relative flex w-full flex-col gap-2.5 overflow-auto isolate",
                 className,
             )}
             {...props}
         >
             {children}
+
             <ScrollArea className="whitespace-nowrap">
                 <Table>
                     <TableHeader>
@@ -108,6 +111,20 @@ export function DataTable<TData>({
                 </Table>
                 <ScrollBar orientation="horizontal" className="hidden" />
             </ScrollArea>
+
+            {table.getFilteredSelectedRowModel().rows.length > 0 && (
+                <div className="fixed bottom-1/10 left-1/2 w-fit bg-white rounded-(--card-radius) p-(--card-padding) z-10 [--card-radius:calc(var(--radius)-2px)] [--card-padding:--spacing(1)]">
+                    <Button
+                        variant="ghost"
+                        className="rounded-[calc(var(--card-radius)-var(--card-padding))]"
+                    >
+                        <Trash2 className="text-cherry-red" />
+                        {table.getFilteredSelectedRowModel().rows.length === 1
+                            ? "Blacklist user"
+                            : "Blacklist users"}
+                    </Button>
+                </div>
+            )}
 
             <div className="flex flex-col gap-2.5 px-4 pb-8 mt-4">
                 <DataTablePagination table={table} />
